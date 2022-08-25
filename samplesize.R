d212c3e484417406fbba82b2e9ab8181e2bbf34d
Damiano C <51063036+damiacer@users.noreply.github.com>
2022-08-25 16:40:12 +0200

    Add files via upload

---
+++ samplesize.R
@@ -0,0 +1,13 @@
+library("presize")
+
+prec_prop(p = 0.17, 
+n = NULL, conf.width = 0.1, conf.level = 0.95, method = "exact")
+
+
+#     sample size for a proportion with exact confidence interval. 
+#
+#     p padj        n conf.width conf.level       lwr       upr
+#1 0.17   NA 234.8403        0.1       0.95 0.1242729 0.2242729
+#
+#NOTE: padj is the adjusted proportion, from which the ci is calculated.
+
