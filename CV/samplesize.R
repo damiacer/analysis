f1fd9d39dac8f358166589d24894a846601845dd
Damiano C <51063036+damiacer@users.noreply.github.com>
2022-10-10 17:09:45 +0200

    Update samplesize.R

--- CV/samplesize.R
+++ CV/samplesize.R
@@ -3,10 +3,10 @@ library("pwr")
 # 1st = extremely small Cohen's d and 90% power
 
 pwr.t.test(d=0.1337089,
-	power=0.9,
-	sig.level=0.05,
-	type="one.sample",
-	alternative="two.sided")
+           power=0.9,
+           sig.level=0.05,
+           type="one.sample",
+           alternative="two.sided")
 
 # One-sample t test power calculation 
 #
@@ -17,20 +17,19 @@ pwr.t.test(d=0.1337089,
 #    alternative = two.sided
 
 # ARE (Asymptotic Relative Efficiency)
-589.6508/0.846 
-N = 696.9868
+N = 589.6508/0.864 
 # Taking into account a 20 p100 Lost to Follow-Up
-N + (N * 0.20) # = 836.3842
+N + (N * 0.20) # = 818.9594
 
 #---------------------------------------------------------------
 
 # 2nd = extremely small Cohen's d and 80% power
 
 pwr.t.test(d=0.1337089,
-	power=0.8,
-	sig.level=0.05,
-	type="one.sample",
-	alternative="two.sided")
+           power=0.8,
+           sig.level=0.05,
+           type="one.sample",
+           alternative="two.sided")
 
 # One-sample t test power calculation 
 #
@@ -40,19 +39,18 @@ pwr.t.test(d=0.1337089,
 #          power = 0.8
 #    alternative = two.sided
 
-440.9463/0.846
-N = 521.2131
-N + (N * 0.20) # = 625.4557
+N = 440.9463/0.864
+N + (N * 0.20) # = 612.4254
 
 #---------------------------------------------------------------
 
 # 3rd = bigger Cohen's d (0.2) with 90% power
 
 pwr.t.test(d=0.2,
-	power=0.9,
-	sig.level=0.05,
-	type="one.sample",
-	alternative="two.sided")
+           power=0.9,
+           sig.level=0.05,
+           type="one.sample",
+           alternative="two.sided")
 
 # One-sample t test power calculation 
 #
@@ -62,19 +60,19 @@ pwr.t.test(d=0.2,
 #          power = 0.9
 #    alternative = two.sided
 
-264.6137/0.846
-N = 312.7822
-N + (N * 0.20) # = 375.3386
+
+N = 264.6137/0.864
+N + (N * 0.20) # = 367.519
 
 #---------------------------------------------------------------
 
 # 4rd = largest Cohen's d (0.5) with 90% power
 
 pwr.t.test(d=0.5,
-	power=0.9,
-	sig.level=0.05,
-	type="one.sample",
-	alternative="two.sided")
+           power=0.9,
+           sig.level=0.05,
+           type="one.sample",
+           alternative="two.sided")
 
 # One-sample t test power calculation 
 #
@@ -84,6 +82,6 @@ pwr.t.test(d=0.5,
 #          power = 0.9
 #    alternative = two.sided
 
-43.99548/0.846
-N = 52.00411
-N + (N * 0.20) # = 62.40493
+
+N = 43.99548/0.864
+N + (N * 0.20) # = 61.10483
