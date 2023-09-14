778f464e3de453a5c0421771c71fbfe91e79584d
Damiano C <51063036+damiacer@users.noreply.github.com>
2023-09-14 10:15:06 +0200

    Update samplesize.R

--- BenbS/samplesize.R
+++ BenbS/samplesize.R
@@ -19,3 +19,20 @@ power_t_test(
 ),
   strict = TRUE
 )
+
+
+#    Two-sample t test power calculation with unequal sample sizes and unequal variances 
+#
+#              n = 32.22229, 161.11147
+#          delta = 13.75197
+#             sd = 16.33323, 38.94822
+#      sig.level = 0.05
+#          power = 0.9
+#    alternative = two.sided
+#
+#NOTE: n is vector of number in each group
+#
+#> 32.22229+161.11147
+#[1] 193.3338
+#> 161.1114/32.2222
+#[1] 5.000012
