2f2967b4894f3bb685e43885d25666f06b922212
Damiano C <51063036+damiacer@users.noreply.github.com>
2023-11-10 13:27:09 +0100

    Update samplesize.R with the new sample size

--- MS_GC/samplesize.R
+++ MS_GC/samplesize.R
@@ -5,8 +5,20 @@ mean(x)
 sd(x)
 
 y=c(40,33,0,33,9,0,0,82,37,33,35,33,0,13,40,41)
+mean(y)
 sd(y)
-sd(y)
+
+mean(x)-mean(y) # 20.625
+
+# common sd = sqrt(s1^2/n1 + s2^2/n2)
+
+sdx = sd(x)
+sdy = sd(y)
+csd = sqrt((sdx^2)/16 + (sdy^2)/16)
+csd2 = sqrt(sdx^2 + sdy^2)
+
+d = x-y
+sdd = sd(d)
 
 M1  = mean(x) # Mean for sample 1
 M2  = mean(y) # Mean for sample 2
@@ -38,3 +50,19 @@ pwr.t.test(
 29.20669*2 # = 58.41338
 58.41338/0.864 # = 67.60808 ~ 68
 
+# sample size for superiority trial
+
+install.packages("SampleSize4ClinicalTrials")
+require("SampleSize4ClinicalTrials")
+
+# 2L for superiority trials
+# ssc_meancomp(design, ratio, alpha, power, sd, theta, delta)
+
+ssc_meancomp(design = 2L, ratio = 1, 
+		alpha = 0.05, power = 0.8, sd = 8.448958, theta = 20.625, delta = 15)
+
+
+ssc_meancomp(design = 2L, ratio = 1, 
+		alpha = 0.05, power = 0.8, sd = 16.76455, theta = 20.625, delta = 10)
+31*2
+62/0.864 # 71.75926
