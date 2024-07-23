bca54033b9850e3409b866565d15f7d976b04d61
Damiano C <51063036+damiacer@users.noreply.github.com>
2024-07-23 13:35:52 +0200

    Update NSN.R

--- MiGEPS/NSN.R
+++ MiGEPS/NSN.R
@@ -1,5 +1,4 @@
 install.packages("pwr")
-
 require("pwr")
 
 m1 = 27.0
@@ -16,11 +15,24 @@ pwr.t.test(n = NULL,
 			type = "paired",
 			alternative = "two.sided"
 			)
+
+#      Paired t test power calculation 
+#
+#              n = 339.0313
+#              d = 0.152588
+#      sig.level = 0.05
+#          power = 0.8
+#    alternative = two.sided
+#
+#NOTE: n is number of *pairs*
 			
-diffmean = abs(m1 - m2)
-comsd = sqrt((s1^2 + s2^2)/2)
 
 install.packages("presize")
 require("presize")
 
 prec_mean(m = 29.8, sd = 18.4, n = NULL, conf.width = 0.2, conf.level = 0.95)
+
+#     sample size for mean 
+#
+#  mean   sd        n conf.width conf.level  lwr  upr
+#  29.8 18.4 130058.9        0.2       0.95 29.7 29.9
